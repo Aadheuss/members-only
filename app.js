5e6ddbd3542b044a9c08390111f07852e45976a7
Aadheuss <aadheusx@gmail.com>
2024-03-12 21:12:51 +0700

    Set up database connection

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI;
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+main().catch((err) => console.log(err));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
