f24750176b0f9679c70baf6555b1dc8c7c9445fb
Aadheuss <aadheusx@gmail.com>
2024-03-16 01:46:02 +0700

    Implement index route to show all messages

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
-const passport = require("../config/passport");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Members only", user: req.user });
+// Require models
+const User = require("../models/user");
+const Message = require("../models/message");
+
+const indexController = asyncHandler(async (req, res, next) => {
+  const allMessages = await Message.find({})
+    .sort({ timestamp: 1 })
+    .populate("author")
+    .exec();
+
+  res.render("index", {
+    title: "Members only",
+    user: req.user,
+    message_list: allMessages,
+  });
 });
+/* GET home page. */
+router.get("/", indexController);
 
 module.exports = router;
